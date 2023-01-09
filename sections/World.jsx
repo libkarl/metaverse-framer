fbece3b2f4e308050cc318ebf08fe11adc07c8f8
libkarl <karel.hb@email.cz>
2023-01-09 19:18:20 +0100

    init

---
+++ sections/World.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { TitleText, TypingText } from "../components";
+import { fadeIn, staggerContainer } from "../utils/motion";
+
+const World = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| People on the World" textStyles="text-center" />
+      <TitleText
+        title={
+          <>
+            Track friends around you and invite them to play together in the
+            same world
+          </>
+        }
+        textStyles="text-center"
+      />
+    </motion.div>
+  </section>
+);
+
+export default World;
